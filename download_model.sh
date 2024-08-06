ef9b864a97b6d166b1726db1d8c8773a02003afd
Hassan-jr-2 <abdiladifhassan115@gmail.com>
2024-08-06 12:12:18 +0300

    v8

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p /src/base
-wget -O /src/base "https://huggingface.co/SG161222/RealVisXL_V4.0/resolve/main/RealVisXL_V4.0.safetensors"
+wget -O /src/base/RealVisXL_V4.0.safetensors "https://huggingface.co/SG161222/RealVisXL_V4.0/resolve/main/RealVisXL_V4.0.safetensors"
